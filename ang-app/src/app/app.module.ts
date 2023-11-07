e6cb0d1c1317bb0145cc164eb49bb5cd6ff7365c
and96437345 <and96437345@yandex.ru>
2023-11-07 22:29:53 +0300

    Angular_1

---
+++ ang-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutMeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
